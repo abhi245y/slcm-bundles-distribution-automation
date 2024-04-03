740ff67aab51aa4b2340234e95fe3dd7d3cd6688
Abhiram <abhi245y@gmail.com>
2024-04-03 21:47:03 +0530

    fixed issue where styling was not getting applied

--- labs/testing_styling_sorting_excel.py
+++ labs/testing_styling_sorting_excel.py
@@ -2,7 +2,7 @@ import pandas as pd
 import string
 
 
-df = pd.read_excel('./Inputs/Grabbed_Results/Second Semester PG November 2023/Second Semester PG November 2023 R 7558.xlsx')
+df = pd.read_excel(r'Second Semester PG November 2023 R 7558.xlsx')
 
 def apply_styles(worksheet, df, workbook):
     worksheet.set_column('A:A', 42 / 7)  # Sl.No.
@@ -15,16 +15,14 @@ def apply_styles(worksheet, df, workbook):
 
     worksheet.set_default_row(39)
 
-    # Set cell alignment and wrap text
     align_format = workbook.add_format({
         'align': 'center',
         'valign': 'vcenter',
         'text_wrap': True
     })
 
-    worksheet.set_margins(left=0.3, right=0.3, top=0.6, bottom=0.3)
+    worksheet.set_margins(left=0.25, right=0.25, top=0.6, bottom=0.3)
 
-    # Add header
     header_format = workbook.add_format({
         'font_size': 15,
         'bold': True,
@@ -32,35 +30,16 @@ def apply_styles(worksheet, df, workbook):
     })
     worksheet.set_header('&C&"Arial"&BSecond Semester PG November 2023&"', {'header_header_spacing': 0.3, 'font': header_format})
 
-    # Write the header row
-    for col_num, col_name in enumerate(df.columns):
-        worksheet.write(0, col_num, col_name, align_format)
-
     # Apply alignment and wrap text to data cells
     for row_num, row_data in enumerate(df.values):
         for col_num, cell_value in enumerate(row_data):
             worksheet.write(row_num + 1, col_num, str(cell_value), align_format)
     
-        # Enable "Columns to Repeat at Top"
-    worksheet.repeat_rows(0)  # Repeat the first row (header row)
+    worksheet.repeat_rows(0)  
 
-    # Apply "Format as Table" style
-    table_format = workbook.add_format({
-        'bg_color': '#a8a7a7',
-        'border': 3
-    })
-    worksheet.add_table(f'A1:{get_column_letter(len(df.columns))}1', {'style': 'Table Style Light 1', 'banded_rows': True, 'columns': [{'header': col} for col in df.columns]})
-    worksheet.autofilter(0, 0, len(df) - 1, len(df.columns) - 1)
-
-# Helper function to get column letter from column index
-def get_column_letter(column_index):
-    column_letter = ''
-    while column_index >= 0:
-        column_index, remainder = divmod(column_index, 26)
-        column_letter = string.ascii_uppercase[remainder] + column_letter
-        column_index -= 1
-    return column_letter
-
+    (max_row, max_col) = df.shape
+    column_settings = [{"header": column} for column in df.columns]
+    worksheet.add_table(0, 0, max_row, max_col - 1, {"columns": column_settings})
 
 writer = pd.ExcelWriter('merged_file.xlsx', engine='xlsxwriter')
 workbook = writer.book
@@ -82,4 +61,4 @@ for camp in df['Camp'].unique():
         apply_styles(worksheet, new_df, workbook)
 
 
-writer.save()
+writer.close()
